242b69041f9e2695a92a5bbea0cf4d3e27929983
conor-keane <g00391846@atu.ie>
2022-12-07 11:37:54 +0000

    Set up values for Employee

--- src/main/java/ie/atu/Employee.java
+++ src/main/java/ie/atu/Employee.java
@@ -1,4 +1,9 @@
 package ie.atu;
 
 public class Employee {
+    private String name;
+    private String ppsId;
+    private String employmentType;
+    private String gender;
+    private int age;
 }
