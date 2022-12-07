d10ebf94bdb7ecdeb3d95cfd6d5493409be30505
conor-keane <g00391846@atu.ie>
2022-12-07 11:48:22 +0000

    Made a hard coded in Employee to run tests with

--- src/test/java/ie/atu/EmployeeTestSuccess.java
+++ src/test/java/ie/atu/EmployeeTestSuccess.java
@@ -1,7 +1,18 @@
 package ie.atu;
 
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTestSuccess {
+
+    private Employee myEmployee;
+
+    @BeforeEach
+    void setUp() {
+        myEmployee = new Employee("Conor", "1234567891011", "Full-time", "Male", 21); {
+
+        }
+    }
 
 }
