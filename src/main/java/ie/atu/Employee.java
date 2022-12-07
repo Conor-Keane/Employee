68ae815293927cdb61c069d27141be8ea8f92304
conor-keane <g00391846@atu.ie>
2022-12-07 11:40:41 +0000

    Added constructor to Employee class

--- src/main/java/ie/atu/Employee.java
+++ src/main/java/ie/atu/Employee.java
@@ -6,4 +6,12 @@ public class Employee {
     private String employmentType;
     private String gender;
     private int age;
+
+    public Employee(String name, String ppsId, String employmentType, String gender, int age) {
+        this.name = name;
+        this.ppsId = ppsId;
+        this.employmentType = employmentType;
+        this.gender = gender;
+        this.age = age;
+    }
 }
